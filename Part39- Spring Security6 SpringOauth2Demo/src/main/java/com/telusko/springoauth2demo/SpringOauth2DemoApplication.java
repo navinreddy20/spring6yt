e332ab0bd5b482ab095744589bb7ec1681b1ed83
Gaurav Sharma <gsharma1154@gmail.com>
2024-08-11 17:58:41 +0530

    oauth2 code added

---
+++ Part39- Spring Security6 SpringOauth2Demo/src/main/java/com/telusko/springoauth2demo/SpringOauth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.springoauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2DemoApplication.class, args);
+	}
+
+}
